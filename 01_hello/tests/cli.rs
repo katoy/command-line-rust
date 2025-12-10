d587871d87172f366ce6bb00f29650e68828df7e
katoy <youichikato@gmail.com>
2025-12-11 04:30:24 +0900

    chore: 練習用の false/true コマンドと関連テストを削除
    練習用の課題として作成されていた src/bin/false.rs と src/bin/true.rs は、Hello World プロジェクトの構成としては不要となったため削除しました。これに伴い、tests/cli.rs から関連するテストケースも削除しています。

--- 01_hello/tests/cli.rs
+++ 01_hello/tests/cli.rs
@@ -9,15 +9,3 @@ fn runs() {
     let stdout = String::from_utf8(output.stdout).expect("invalid UTF-8");
     assert_eq!(stdout, "Hello, world!\n");
 }
-
-#[test]
-fn true_ok() {
-    let mut cmd = Command::new(assert_cmd::cargo::cargo_bin!("true"));
-    cmd.assert().success();
-}
-
-#[test]
-fn false_not_ok() {
-    let mut cmd = Command::new(assert_cmd::cargo::cargo_bin!("false"));
-    cmd.assert().failure();
-}
